8db21d2a31cb8467c02405efa8c4d844ef51b98f
jorge.vargas <jorge.vargas@homeadvisor.com>
2020-07-30 18:23:21 -0500

    call rest api using imputstream and http

--- src/main/java/com/example/demo/controllers/ClientController.java
+++ src/main/java/com/example/demo/controllers/ClientController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import com.example.demo.beans.BookResponse;
 import com.example.demo.clients.BookClientFeign;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -11,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicReference;
@@ -28,6 +35,68 @@ public class ClientController {
     public String helloWorld(){
         return "Hello World!";
     }
+
+    @GetMapping (value = "/stream")
+    public BookResponse stream()  {
+        Gson g = new GsonBuilder().setLenient().create();
+
+        AtomicReference<BookResponse> bookResponse=new AtomicReference<>();
+        StringBuffer response=new StringBuffer();
+        IntStream.range(1, 10).forEach(
+                num -> {
+                    try {
+                        callUsingSream(g, bookResponse, response);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        return bookResponse.get();
+    }
+
+    private void callUsingSream(Gson g, AtomicReference<BookResponse> bookResponse, StringBuffer response) throws IOException {
+        URL urlForGetRequest = new URL(URL_ENDPOINT+ new Random().nextInt(100));
+
+        String readLine = null;
+
+        HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
+
+        conection.setRequestMethod("GET");
+
+        //conection.setRequestProperty("userId", "a1bcdef"); // set userId its a sample here
+
+        int responseCode = conection.getResponseCode();
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+
+            BufferedReader in = new BufferedReader(
+
+                    new InputStreamReader(conection.getInputStream()));
+
+
+
+            while ((readLine = in .readLine()) != null) {
+
+                response.append(readLine);
+
+            } in .close();
+
+            // print result
+
+            log.debug("JSON String Result " + response.toString());
+            try {
+                bookResponse.set(g.fromJson(response.toString(), BookResponse.class));
+            }catch (RuntimeException ex){
+                log.error("Problem translating data.. GSON.. {}",ex.getMessage());
+            }
+
+        } else {
+
+            log.error("GET NOT WORKED");
+
+        }
+    }
+
 
     @GetMapping (value = "/restTemplate")
     public BookResponse getBooks(){

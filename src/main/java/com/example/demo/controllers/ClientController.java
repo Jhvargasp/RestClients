e07f8ff471ab54304b84dc17f97af9d36fc9dcb8
jorge.vargas <jorge.vargas@homeadvisor.com>
2020-07-30 17:54:02 -0500

    Spring boot empty project only one controller

--- src/main/java/com/example/demo/controllers/ClientController.java
+++ src/main/java/com/example/demo/controllers/ClientController.java
@@ -1,18 +1,10 @@
 package com.example.demo.controllers;
 
-import com.example.demo.beans.BookResponse;
+
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.Arrays;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping("client")
@@ -28,22 +20,7 @@ public class ClientController {
     }
 
 
-    @GetMapping (value = "/webClient")
-    public BookResponse getBooks2(){
-        AtomicReference<BookResponse> lastResponse=new AtomicReference<>();
-        IntStream.range(1, 10).forEach(
-                num -> {
-                    WebClient client2 = WebClient.create(URL_ENDPOINT + new Random().nextInt(100));
-                    BookResponse bookResponse= client2.get().retrieve().bodyToMono(BookResponse.class).block();
-                    lastResponse.set(bookResponse);
-                    log.debug("Got {} books", bookResponse.getTotalItems());
 
-                    Arrays.stream(bookResponse.getItems())//.filter(x -> Float.parseFloat(Optional.ofNullable(x.getVolumeInfo().getAverageRating()).orElse("0.0")) >= 2.0f)
-                            .forEach(x -> log.debug("Name  {}, rating {} ", x.getVolumeInfo().getTitle(), x.getVolumeInfo().getAverageRating()));
-                }
-        );
-        return lastResponse.get();
-    }
 
 
 
